615b9f03c8f03b1ce8b1f8a3015753d6acfd73fa
ViniMacacari <macacarivinicius@gmail.com.br>
2024-10-29 14:05:41 -0300

    Not found

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter, RouterModule, withInMemoryScrolling } from '@angular/router'
+import { provideHttpClient } from '@angular/common/http'
+import { ɵLOTTIE_OPTIONS } from 'ngx-lottie'
+import player from 'lottie-web'
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'
+import { RestaurarPosicaoService } from './services/router/restaurar-posicao.service'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })
+    ),
+    provideHttpClient(),
+    { provide: ɵLOTTIE_OPTIONS, useValue: { player: () => player } },
+    RestaurarPosicaoService
+  ]
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router'
 
 import { HomeComponent } from './pages/home/home.component'
 
+import { NotFoundComponent } from './pages/not-found/not-found.component'
+
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+
+    { path: '404', component: NotFoundComponent },
+    { path: '**', redirectTo: '/404' }
 ]
