02063492fb8e0f643d615c2f18928078dc1d7d21
ViniMacacari <macacarivinicius@gmail.com.br>
2024-10-29 13:26:14 -0300

    Base do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { MenuComponent } from "./components/menu/menu.component"
+import { FooterComponent } from "./components/footer/footer.component"
+import { ModalLoadingComponent } from "./components/modal-loading/modal-loading.component"
+import { ModalComponent } from "./components/modal/modal.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, FooterComponent, ModalLoadingComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'adoremus';
+  title = 'adoremus'
 }
---
+++ src/app/components/modal-loading/modal-loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-loading',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-loading.component.html',
+  styleUrl: './modal-loading.component.scss'
+})
+export class ModalLoadingComponent {
+
+}
