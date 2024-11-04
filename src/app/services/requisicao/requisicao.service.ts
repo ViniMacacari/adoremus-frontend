0c97290ee69a5c1df887086bcaba889e9d364d55
ViniMacacari <macacarivinicius@gmail.com.br>
2024-11-04 13:30:33 -0300

    Serviço de requisição

---
+++ src/app/services/requisicao/requisicao.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http'
+import { Observable, throwError, firstValueFrom } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequisicaoService {
+
+  private url = 'https://api.adoremus.com.br'
+
+  constructor(private http: HttpClient) { }
+
+  async get(complemento: string, responseType?: 'blob', options?: any): Promise<any> {
+    const headers = this.createHeaders()
+    const config: any = { headers, withCredentials: true }
+
+    if (responseType === 'blob') {
+      config.responseType = 'blob'
+    }
+
+    if (options) {
+      Object.assign(config, options)
+    }
+
+    return firstValueFrom(
+      this.http.get(`${this.url}${complemento}`, config).pipe(
+        catchError(this.handleError)
+      )
+    )
+  }
+
+  async post(complemento: string, body: any): Promise<any> {
+    const headers = this.createHeaders()
+    return firstValueFrom(
+      this.http.post(`${this.url}${complemento}`, body, { headers, withCredentials: true }).pipe(
+        catchError(this.handleError)
+      )
+    )
+  }
+
+  async put(complemento: string, body: any): Promise<any> {
+    const headers = this.createHeaders()
+    return firstValueFrom(
+      this.http.put(`${this.url}${complemento}`, body, { headers, withCredentials: true }).pipe(
+        catchError(this.handleError)
+      )
+    )
+  }
+
+  async delete(complemento: string): Promise<any> {
+    const headers = this.createHeaders()
+    return firstValueFrom(
+      this.http.delete(`${this.url}${complemento}`, { headers, withCredentials: true }).pipe(
+        catchError(this.handleError)
+      )
+    )
+  }
+
+  private createHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'token': `${localStorage.getItem('token')}`,
+      'grupos': `${localStorage.getItem('grupos')}`,
+      'user': `${localStorage.getItem('user')}`
+    })
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Erro: ${error.error.error}`
+    } else {
+      errorMessage = `${error.error.error}`
+    }
+    return throwError(errorMessage)
+  }
+}
