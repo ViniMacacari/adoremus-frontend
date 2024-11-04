89b530fc49f482ce3fc54c846bf165dfdd127e3f
ViniMacacari <macacarivinicius@gmail.com>
2024-11-04 19:50:47 -0300

    Serviço para restaurar posição da tela ao trocar de página

--- src/app/services/router/restaurar-posicao.service.ts
+++ src/app/services/router/restaurar-posicao.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
+import { ViewportScroller } from '@angular/common'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RestaurarPosicaoService {
-
-  constructor() { }
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0])
+      }
+    })
+  }
 }
