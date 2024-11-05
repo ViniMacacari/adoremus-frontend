eaa223d62b1e6bc5aafffadfad2432bc4b6de34f
ViniMacacari <macacarivinicius@gmail.com.br>
2024-11-05 12:34:39 -0300

    Navegando até o site do Santo Terco

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
-
+  navegarExterno(url: string): void {
+    window.open(url, '_blank')
+  }
 }
