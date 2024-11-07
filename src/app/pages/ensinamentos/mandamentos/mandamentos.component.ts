903169c5e9c23e31c0909372f21ac53b01957fe5
ViniMacacari <macacarivinicius@gmail.com>
2024-11-06 21:35:20 -0300

    Rotas de mandamentos

---
+++ src/app/pages/ensinamentos/mandamentos/mandamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mandamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './mandamentos.component.html',
+  styleUrl: './mandamentos.component.scss'
+})
+export class MandamentosComponent {
+
+}
