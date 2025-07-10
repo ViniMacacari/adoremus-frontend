8cd9efaf75573a9c3977d8669fbe6b8f04c3fdf5
ViniMacacari <macacarivinicius@gmail.com>
2025-07-10 10:11:08 -0300

    feat: adaptação da página para oração em português e latim juntas

--- src/app/pages/prayers/prayers.component.ts
+++ src/app/pages/prayers/prayers.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RequestService } from '../../services/requisicao/requisicao.service'
 import { ButtonComponent } from "../../components/button/button.component"
+import { LoaderComponent } from "../../components/loader/loader.component"
 
 @Component({
   selector: 'app-prayers',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, LoaderComponent],
   templateUrl: './prayers.component.html',
   styleUrl: './prayers.component.scss'
 })
 export class PrayersComponent {
   prayers: any[] = []
   allLoaded: boolean = false
+  selectedPrayer: any = {}
 
   constructor(
     private request: RequestService
@@ -33,5 +35,13 @@ export class PrayersComponent {
     } catch (error) {
       console.error('Error fetching prayers:', error)
     }
+  }
+
+  selectPrayer(event: any): void {
+    this.selectedPrayer = event
+
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }, 0)
   }
 }
