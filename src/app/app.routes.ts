d864b531091a9841d31fea989a67cda0b758f085
ViniMacacari <macacarivinicius@gmail.com>
2025-07-09 23:10:02 -0300

    feat: criação de componente de calendario liturgico e navegação entre tela principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './pages/home/home.component'
-import { CalendarioLiturgicoComponent } from './pages/calendario-liturgico/calendario-liturgico.component'
-
-import { MandamentosComponent } from './pages/ensinamentos/mandamentos/mandamentos.component'
+import { LiturgicalCalendarComponent } from './pages/liturgical-calendar/liturgical-calendar.component'
 
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'ano-liturgico', component: CalendarioLiturgicoComponent },
-
-    { path: 'ensinamentos/mandamentos', component: MandamentosComponent },
+    { path: 'ano-liturgico', component: LiturgicalCalendarComponent },
 
     { path: '404', component: NotFoundComponent },
     { path: '**', redirectTo: '/404' }
---
+++ src/app/pages/liturgical-calendar/liturgical-calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liturgical-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './liturgical-calendar.component.html',
+  styleUrl: './liturgical-calendar.component.scss'
+})
+export class LiturgicalCalendarComponent {
+
+}
