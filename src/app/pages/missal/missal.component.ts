a9ca2b3c1472c34173c6ce5bbe261a10eb9c62d9
ViniMacacari <macacarivinicius@gmail.com>
2025-08-10 22:54:59 -0300

    feat: missal romano completo com latim e português

--- src/app/pages/missal/missal.component.ts
+++ src/app/pages/missal/missal.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { LoaderComponent } from "../../components/loader/loader.component"
+import { RequestService } from '../../services/requisicao/requisicao.service'
 
 @Component({
   selector: 'app-missal',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent, CommonModule],
   templateUrl: './missal.component.html',
   styleUrl: './missal.component.scss'
 })
 export class MissalComponent {
+  allLoaded: boolean = false
+  missalData: any = {}
 
+  constructor(
+    private request: RequestService
+  ) { }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.loadMissalInfo()
+    })
+  }
+
+  async loadMissalInfo(): Promise<void> {
+    try {
+      const result = await this.request.get('/missal/ordinario')
+      this.missalData = result.dados
+      console.log(this.missalData)
+      this.allLoaded = true
+    } catch (error: any) {
+      console.error(error)
+    }
+  }
 }
