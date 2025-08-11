075969f82d0facac16111db9b36d8f93a16c0b3b
ViniMacacari <macacarivinicius@gmail.com>
2025-08-10 22:44:05 -0300

    feat: componente de missal criado e opção adicionada

---
+++ src/app/pages/missal/missal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missal',
+  standalone: true,
+  imports: [],
+  templateUrl: './missal.component.html',
+  styleUrl: './missal.component.scss'
+})
+export class MissalComponent {
+
+}
