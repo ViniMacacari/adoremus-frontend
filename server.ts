9b232d6e7f9d62ebf2fcf4479941791c9556ba7e
ViniMacacari <macacarivinicius@gmail.com>
2025-11-17 21:56:29 -0300

    fix: correção para salvamento de cache

--- server.ts
+++ server.ts
@@ -1,33 +1,38 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common'
+import { CommonEngine } from '@angular/ssr'
+import express from 'express'
+import { fileURLToPath } from 'node:url'
+import { dirname, join, resolve } from 'node:path'
+import bootstrap from './src/main.server'
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+  const server = express()
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url))
+  const browserDistFolder = resolve(serverDistFolder, '../browser')
+  const indexHtml = join(serverDistFolder, 'index.server.html')
 
-  const commonEngine = new CommonEngine();
+  const commonEngine = new CommonEngine()
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+  server.set('view engine', 'html')
+  server.set('views', browserDistFolder)
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
     maxAge: '1y',
     index: 'index.html',
-  }));
+  }))
 
-  // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    if (req.originalUrl === '/') {
+      const maxage = 60 * 59
+      const SWR_IN_SECONDS = 60 * 60
+
+      res.set({
+        'Cache-Control': `s-maxage=${maxage}, stale-while-revalidate=${SWR_IN_SECONDS}`,
+      })
+    }
+
+    const { protocol, originalUrl, baseUrl, headers } = req
 
     commonEngine
       .render({
@@ -38,20 +43,20 @@ export function app(): express.Express {
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
       .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+      .catch((err) => next(err))
+  })
 
-  return server;
+  return server
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4000
 
   // Start up the Node server
-  const server = app();
+  const server = app()
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    console.log(`Node Express server listening on http://localhost:${port}`)
+  })
 }
 
-run();
+run()
